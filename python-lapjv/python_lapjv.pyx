1ea04e77039d07d3bd5777eaaf4fcdcdcd7fb33d
Tomas Kazmar <tomash.kazmar@seznam.cz>
2012-08-08 15:51:51 +0200

    Add support for non-square matrices and cost limit to LAPJV.

--- python-lapjv/python_lapjv.pyx
+++ python-lapjv/python_lapjv.pyx
@@ -14,41 +14,60 @@ cdef extern from "wrap_lapjv.h":
         double *u, 
         double *v)
 
-def lap(cost):
+def lap(np.ndarray cost not None, extend_cost=False, cost_limit=None):
     '''
     Solve linear assignment problem using Jonker-Volgenant algorithm.
 
-    cost: square matrix containing the assignment costs
+    cost: (square) matrix containing the assignment costs
+    extend_cost: whether or not extend a non-square matrix
+    cost_limit: an upper limit for a cost of a single assignment
 
-    Returns (lapcost, rowsol, colsol, u, v) where:
+    Returns (lapcost, rowsol, colsol) where:
       lapcost: cost of the assignment
       rowsol:  vector of columns assigned to rows
       colsol:  vector of rows assigned to columns
-      u:       row reduction numbers (dual variables)
-      v:       column reduction numbers (dual variables)
+
+    When extend_cost and/or cost_limit is set, all unmatched entries will be
+    marked by -1 in rowsol/colsol.
     '''
     
     if cost.ndim != 2:
         raise ValueError('2-dimensional array expected')
-    if cost.shape[0] != cost.shape[1]:
-        raise ValueError('Square array expected')
+    cdef np.ndarray[np.double_t, ndim=2, mode='c'] cost_c = np.ascontiguousarray(cost, dtype=np.double)
+    cdef np.ndarray[np.double_t, ndim=2, mode='c'] cost_c_extended
+    if cost_c.shape[0] != cost_c.shape[1] and not extend_cost:
+        raise ValueError('No cost extension allowed - square array expected')
+    cdef int N
+    if cost_c.shape[0] != cost_c.shape[1] or cost_limit != None:
+        N = max(cost_c.shape[0], cost_c.shape[1])
+        if cost_limit == None:
+            cost_c_extended = np.zeros((N, N), dtype=np.double)
+        else:
+            cost_c_extended = np.zeros((2*N, 2*N), dtype=np.double) + cost_limit
+        if extend_cost:
+            cost_c_extended[:N, :N] = 2*cost_c.max()
+        cost_c_extended[:cost_c.shape[0], :cost_c.shape[1]] = cost_c
+        cost_c = cost_c_extended
 
-    cdef np.ndarray[np.double_t, ndim=2, mode='c'] cost_c
-    cost_c = np.ascontiguousarray(cost, dtype=np.double)
     cdef int i
     cdef double **cost_ptr
     cost_ptr = <double **> malloc(cost_c.shape[0] * sizeof(double *))
     for i in range(cost_c.shape[0]):
         cost_ptr[i] = &cost_c[i, 0]
 
-    cdef np.ndarray[np.int32_t, ndim=1, mode='c'] rowsol_c = np.zeros((cost.shape[0],), dtype=np.int32)
-    cdef np.ndarray[np.int32_t, ndim=1, mode='c'] colsol_c = np.zeros((cost.shape[0],), dtype=np.int32)
-    cdef np.ndarray[np.double_t, ndim=1, mode='c'] u_c = np.zeros((cost.shape[0],), dtype=np.double)
-    cdef np.ndarray[np.double_t, ndim=1, mode='c'] v_c = np.zeros((cost.shape[0],), dtype=np.double)
+    cdef np.ndarray[np.int32_t, ndim=1, mode='c'] rowsol_c = np.zeros((cost_c.shape[0],), dtype=np.int32)
+    cdef np.ndarray[np.int32_t, ndim=1, mode='c'] colsol_c = np.zeros((cost_c.shape[0],), dtype=np.int32)
+    cdef np.ndarray[np.double_t, ndim=1, mode='c'] u_c = np.zeros((cost_c.shape[0],), dtype=np.double)
+    cdef np.ndarray[np.double_t, ndim=1, mode='c'] v_c = np.zeros((cost_c.shape[0],), dtype=np.double)
 
     ret = _solve(cost_c.shape[0], cost_ptr, &rowsol_c[0], &colsol_c[0], &u_c[0], &v_c[0])
-
     free(cost_ptr)
 
-    return ret, rowsol_c, colsol_c, u_c, v_c
-
+    if cost_limit != None or extend_cost:
+        rowsol_c[rowsol_c >= cost.shape[1]] = -1
+        colsol_c[colsol_c >= cost.shape[0]] = -1
+        rowsol_c = rowsol_c[:cost.shape[0]]
+        colsol_c = colsol_c[:cost.shape[1]]
+        return cost[np.nonzero(rowsol_c != -1)[0], rowsol_c[rowsol_c != -1]].sum(), rowsol_c, colsol_c
+    else:
+        return ret, rowsol_c, colsol_c
--- python-lapjv/test_lap.py
+++ python-lapjv/test_lap.py
@@ -9,25 +9,51 @@ The cost matrix is based on Balas and Toth, 1985, Branch and bound
 Chischester, pp 361--401.
 """
 
-def test_lap():
-    import numpy as np
-    import lapjv
+from nose.tools import raises
 
-    large = 1000
-    cost = np.array( [[large,2,11,10,8,7,6,5],
-                      [6,large,1,8,8,4,6,7],
-                      [5,12,large,11,8,12,3,11],
-                      [11,9,10,large,1,9,8,10],
-                      [11,11,9,4,large,2,10,9],
-                      [12,8,5,2,11,large,11,9],
-                      [10,11,12,10,9,12,large,3],
-                      [10,10,10,10,6,3,1,large]] )
+import numpy as np
+import lapjv
 
+large = 1000
+cost = np.array( [[large,2,11,10,8,7,6,5],
+                  [6,large,1,8,8,4,6,7],
+                  [5,12,large,11,8,12,3,11],
+                  [11,9,10,large,1,9,8,10],
+                  [11,11,9,4,large,2,10,9],
+                  [12,8,5,2,11,large,11,9],
+                  [10,11,12,10,9,12,large,3],
+                  [10,10,10,10,6,3,1,large]] )
+
+def test_lap_square():
     ret = lapjv.lap(cost)
-
     assert ret[0] == 17.0
     assert np.all(ret[1] == [1, 2, 0, 4, 5, 3, 7, 6])
     assert np.all(ret[2] == [2, 0, 1, 5, 3, 4, 7, 6])
-
     assert cost[range(cost.shape[0]), ret[1]].sum() == ret[0]
     assert cost[ret[2], range(cost.shape[1])].sum() == ret[0]
+
+@raises(ValueError)
+def test_lap_empty():
+    lapjv.lap(np.ndarray([]))
+
+@raises(ValueError)
+def test_lap_non_square_fail():
+    lapjv.lap(np.zeros((3, 2)))
+
+def test_lap_non_contigous():
+    ret = lapjv.lap(cost[:3, :3])
+    assert ret[0] == 8.0
+    assert np.all(ret[1] == [1, 2, 0])
+    assert np.all(ret[2] == [2, 0, 1])
+
+def test_lap_extension():
+    ret = lapjv.lap(cost[:2, :4], extend_cost=True)
+    assert ret[0] == 3.0
+    assert np.all(ret[1] == [1, 2])
+    assert np.all(ret[2] == [-1, 0, 1, -1])
+
+def test_lap_cost_limit():
+    ret = lapjv.lap(cost[:3, :3], cost_limit=4.99)
+    assert ret[0] == 3.0
+    assert np.all(ret[1] == [1, 2, -1])
+    assert np.all(ret[2] == [-1, 0, 1])
