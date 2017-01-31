4a6478d1699a4251f89029d8c69213636de2ea3e
Tomas Kazmar <tomash.kazmar@seznam.cz>
2017-01-31 18:12:56 +0100

    Style.

--- lapjv/_lapjv.pyx
+++ lapjv/_lapjv.pyx
@@ -12,20 +12,24 @@ ELSE:
     from libc.stdint cimport int32_t
 
 cdef extern from "internal/lap.h":
-    double lap_internal(int dim,
-        double **assigncost,
-        int32_t *rowsol,
-        int32_t *colsol,
-        double *u,
-        double *v)
+    double lap_internal(
+            int dim,
+            double **assigncost,
+            int32_t *rowsol,
+            int32_t *colsol,
+            double *u,
+            double *v)
 
-def lapjv(cnp.ndarray cost not None, char extend_cost=False, double cost_limit=np.inf):
+
+def lapjv(cnp.ndarray cost not None, char extend_cost=False,
+          double cost_limit=np.inf):
     '''
     Solve linear assignment problem using Jonker-Volgenant algorithm.
 
     cost: (square) matrix containing the assignment costs
     extend_cost: whether or not extend a non-square matrix [default: False]
-    cost_limit: an upper limit for a cost of a single assignment [default: np.inf]
+    cost_limit: an upper limit for a cost of a single assignment
+                [default: np.inf]
 
     Returns (lapcost, rowsol, colsol) where:
       lapcost: cost of the assignment
@@ -35,15 +39,16 @@ def lapjv(cnp.ndarray cost not None, char extend_cost=False, double cost_limit=n
     When extend_cost and/or cost_limit is set, all unmatched entries will be
     marked by -1 in rowsol/colsol.
     '''
-    
     if cost.ndim != 2:
         raise ValueError('2-dimensional array expected')
-    cdef cnp.ndarray[cnp.double_t, ndim=2, mode='c'] cost_c = np.ascontiguousarray(cost, dtype=np.double)
+    cdef cnp.ndarray[cnp.double_t, ndim=2, mode='c'] cost_c = \
+        np.ascontiguousarray(cost, dtype=np.double)
     cdef cnp.ndarray[cnp.double_t, ndim=2, mode='c'] cost_c_extended
     cdef int N = max(cost_c.shape[0], cost_c.shape[1])
     if cost_c.shape[0] != cost_c.shape[1]:
         if not extend_cost:
-            raise ValueError('Square cost array expected. If intentional, pass extend_cost=True.')
+            raise ValueError('Square cost array expected. If intentional, '
+                             'pass extend_cost=True.')
         if cost_limit < np.inf:
             cost_c_extended = np.empty((2*N, 2*N), dtype=np.double)
             cost_c_extended[:] = cost_limit
@@ -65,12 +70,17 @@ def lapjv(cnp.ndarray cost not None, char extend_cost=False, double cost_limit=n
     for i in range(cost_c.shape[0]):
         cost_ptr[i] = &cost_c[i, 0]
 
-    cdef cnp.ndarray[cnp.int32_t, ndim=1, mode='c'] rowsol_c = np.zeros((cost_c.shape[0],), dtype=np.int32)
-    cdef cnp.ndarray[cnp.int32_t, ndim=1, mode='c'] colsol_c = np.zeros((cost_c.shape[0],), dtype=np.int32)
-    cdef cnp.ndarray[cnp.double_t, ndim=1, mode='c'] u_c = np.zeros((cost_c.shape[0],), dtype=np.double)
-    cdef cnp.ndarray[cnp.double_t, ndim=1, mode='c'] v_c = np.zeros((cost_c.shape[0],), dtype=np.double)
+    cdef cnp.ndarray[cnp.int32_t, ndim=1, mode='c'] rowsol_c = \
+        np.zeros((cost_c.shape[0],), dtype=np.int32)
+    cdef cnp.ndarray[cnp.int32_t, ndim=1, mode='c'] colsol_c = \
+        np.zeros((cost_c.shape[0],), dtype=np.int32)
+    cdef cnp.ndarray[cnp.double_t, ndim=1, mode='c'] u_c = \
+        np.zeros((cost_c.shape[0],), dtype=np.double)
+    cdef cnp.ndarray[cnp.double_t, ndim=1, mode='c'] v_c = \
+        np.zeros((cost_c.shape[0],), dtype=np.double)
 
-    ret = lap_internal(cost_c.shape[0], cost_ptr, &rowsol_c[0], &colsol_c[0], &u_c[0], &v_c[0])
+    ret = lap_internal(cost_c.shape[0], cost_ptr, &rowsol_c[0], &colsol_c[0],
+            &u_c[0], &v_c[0])
     free(cost_ptr)
 
     if cost_limit < np.inf or extend_cost:
@@ -78,6 +88,9 @@ def lapjv(cnp.ndarray cost not None, char extend_cost=False, double cost_limit=n
         colsol_c[colsol_c >= cost.shape[0]] = -1
         rowsol_c = rowsol_c[:cost.shape[0]]
         colsol_c = colsol_c[:cost.shape[1]]
-        return cost[np.nonzero(rowsol_c != -1)[0], rowsol_c[rowsol_c != -1]].sum(), rowsol_c, colsol_c
+        return (cost[np.nonzero(rowsol_c != -1)[0],
+                     rowsol_c[rowsol_c != -1]].sum(),
+                rowsol_c,
+                colsol_c)
     else:
         return ret, rowsol_c, colsol_c
--- lapjv/tests/tests.py
+++ lapjv/tests/tests.py
@@ -19,14 +19,15 @@ import os
 from lapjv import lapjv
 
 large = 1000
-cost = np.array( [[large,2,11,10,8,7,6,5],
-                  [6,large,1,8,8,4,6,7],
-                  [5,12,large,11,8,12,3,11],
-                  [11,9,10,large,1,9,8,10],
-                  [11,11,9,4,large,2,10,9],
-                  [12,8,5,2,11,large,11,9],
-                  [10,11,12,10,9,12,large,3],
-                  [10,10,10,10,6,3,1,large]] )
+cost = np.array([[large, 2, 11, 10, 8, 7, 6, 5],
+                 [6, large, 1, 8, 8, 4, 6, 7],
+                 [5, 12, large, 11, 8, 12, 3, 11],
+                 [11, 9, 10, large, 1, 9, 8, 10],
+                 [11, 11, 9, 4, large, 2, 10, 9],
+                 [12, 8, 5, 2, 11, large, 11, 9],
+                 [10, 11, 12, 10, 9, 12, large, 3],
+                 [10, 10, 10, 10, 6, 3, 1, large]])
+
 
 def test_lapjv_square():
     ret = lapjv(cost)
@@ -36,13 +37,16 @@ def test_lapjv_square():
     assert cost[range(cost.shape[0]), ret[1]].sum() == ret[0]
     assert cost[ret[2], range(cost.shape[1])].sum() == ret[0]
 
+
 @raises(ValueError)
 def test_lapjv_empty():
     lapjv(np.ndarray([]))
 
+
 @raises(ValueError)
 def test_lapjv_non_square_fail():
     lapjv(np.zeros((3, 2)))
+
 
 def test_lapjv_non_contigous():
     ret = lapjv(cost[:3, :3])
@@ -50,24 +54,29 @@ def test_lapjv_non_contigous():
     assert np.all(ret[1] == [1, 2, 0])
     assert np.all(ret[2] == [2, 0, 1])
 
+
 def test_lapjv_extension():
     ret = lapjv(cost[:2, :4], extend_cost=True)
     assert ret[0] == 3.0
     assert np.all(ret[1] == [1, 2])
     assert np.all(ret[2] == [-1, 0, 1, -1])
 
+
 def test_lapjv_noextension():
-    c = np.r_[cost[:2, :4], [[1001, 1001, 1001, 2001], [2001, 1001, 1001, 1001]]]
+    c = np.r_[cost[:2, :4],
+              [[1001, 1001, 1001, 2001], [2001, 1001, 1001, 1001]]]
     ret = lapjv(c, extend_cost=False)
     assert ret[0] - 2002 == 3.0
     assert np.all(ret[1] == [1, 2, 0, 3])
     assert np.all(ret[2] == [2, 0, 1, 3])
+
 
 def test_lapjv_cost_limit():
     ret = lapjv(cost[:3, :3], cost_limit=4.99)
     assert ret[0] == 3.0
     assert np.all(ret[1] == [1, 2, -1])
     assert np.all(ret[2] == [-1, 0, 1])
+
 
 def test_lapjv_cost_eps():
     # This test should just return.
