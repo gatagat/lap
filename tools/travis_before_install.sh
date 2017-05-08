75a3cf2bf11da93182c35c2f6dd0d2ac58cc2858
Tomas Kazmar <tomash.kazmar@seznam.cz>
2017-05-08 17:16:25 +0200

    Remove codecov.

--- tools/travis_before_install.sh
+++ tools/travis_before_install.sh
@@ -3,13 +3,13 @@ set -ex
 
 export PIP_DEFAULT_TIMEOUT=60
 
-export TEST_ARGS="-v --ignore-files=^setup.py$ --with-coverage"
+export TEST_ARGS="-v"
 
 section venv
 virtualenv -p python ~/venv
 source ~/venv/bin/activate
 python -m pip install --upgrade pip
-pip install --retries 3 -q wheel codecov pytest pytest-timeout cython numpy
+pip install --retries 3 -q wheel pytest pytest-timeout cython numpy
 pip list
 section_end venv
 
