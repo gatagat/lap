0f297b32ee15879ff36b52538f979a4d00ea405d
Tomas Kazmar <tomash.kazmar@seznam.cz>
2018-07-03 14:03:34 +0200

    Patch tests for MacOS.

--- tools/travis_before_install.sh
+++ tools/travis_before_install.sh
@@ -5,10 +5,23 @@ export PIP_DEFAULT_TIMEOUT=60
 
 export TEST_ARGS="-v"
 
+section install_pip_venv
+python -m pip install -U pip
+if pyver_ge $TRAVIS_PYTHON_VERSION 3.3; then
+	true  # There is the venv module.
+else
+	pip install -U virtualenv
+fi
+section_end install_pip_venv
+
 section venv
-virtualenv -p python ~/venv
+if pyver_ge $TRAVIS_PYTHON_VERSION 3.3; then
+	python -m venv -p python ~/venv
+else
+	virtualenv -p python ~/venv
+fi
 source ~/venv/bin/activate
-python -m pip install --upgrade pip
+python -m pip install -U pip
 pip install --retries 3 -q wheel pytest pytest-timeout cython numpy scipy
 pip list
 section_end venv
--- tools/travis_osx_install.sh
+++ tools/travis_osx_install.sh
@@ -7,5 +7,4 @@ git clone https://github.com/MacPython/terryfy.git ~/terryfy
 source ~/terryfy/travis_tools.sh
 get_python_environment macpython $TRAVIS_PYTHON_VERSION ~/macpython_venv$TRAVIS_PYTHON_VERSION
 source ~/macpython_venv$TRAVIS_PYTHON_VERSION/bin/activate
-pip install virtualenv
 section_end osx_install
