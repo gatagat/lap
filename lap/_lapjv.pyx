cc97e2ebdd2ea275ce94e1a3662b330642005a54
Tomas Kazmar <tomash.kazmar@seznam.cz>
2019-01-27 21:40:54 +0100

    Align non-square matrix handling with lapmod.

--- lap/_lapjv.pyx
+++ lap/_lapjv.pyx
@@ -40,58 +40,70 @@ def lapjv(cnp.ndarray cost not None, char extend_cost=False,
           double cost_limit=np.inf, char return_cost=True):
     """Solve linear assignment problem using Jonker-Volgenant algorithm.
 
-    cost: an N x N matrix containing the assignment costs. Entry cost[i, j] is
-      the cost of assigning row i to column j.
-    extend_cost: whether or not extend a non-square matrix [default: False]
-    cost_limit: an upper limit for a cost of a single assignment
-                [default: np.inf]
-    return_cost: whether or not to return the assignment cost
+    Parameters
+    ----------
+    cost: (N,N) ndarray
+        Cost matrix. Entry `cost[i, j]` is the cost of assigning row `i` to
+        column `j`.
+    extend_cost: bool, optional
+        Whether or not extend a non-square matrix. Default: False.
+    cost_limit: double, optional
+        An upper limit for a cost of a single assignment. Default: `np.inf`.
+    return_cost: bool, optional
+        Whether or not to return the assignment cost.
 
-    Returns (opt, x, y) where:
-      opt: cost of the assignment, not returned if return_cost is False.
-      x: a size-N array specifying to which column each row is assigned.
-      y: a size-N array specifying to which row each column is assigned.
+    Returns
+    -------
+    opt: double
+        Assignment cost. Not returned if `return_cost is False`.
+    x: (N,) ndarray
+        Assignment. `x[i]` specifies the column to which row `i` is assigned.
+    y: (N,) ndarray
+        Assignment. `y[j]` specifies the row to which column `j` is assigned.
 
-    When extend_cost and/or cost_limit is set, all unmatched entries will be
-    marked by -1 in x/y.
+    Notes
+    -----
+    For non-square matrices (with `extend_cost is True`) or `cost_limit` set
+    low enough, there will be unmatched rows, columns in the solution `x`, `y`.
+    All such entries are set to -1.
     """
     if cost.ndim != 2:
         raise ValueError('2-dimensional array expected')
     cdef cnp.ndarray[cnp.double_t, ndim=2, mode='c'] cost_c = \
         np.ascontiguousarray(cost, dtype=np.double)
     cdef cnp.ndarray[cnp.double_t, ndim=2, mode='c'] cost_c_extended
-    cdef int N = max(cost_c.shape[0], cost_c.shape[1])
-    if cost_c.shape[0] != cost_c.shape[1]:
+    cdef uint_t n_rows = cost_c.shape[0]
+    cdef uint_t n_cols = cost_c.shape[1]
+    cdef uint_t n = 0
+    if n_rows == n_cols:
+        n = n_rows
+    else:
         if not extend_cost:
-            raise ValueError('Square cost array expected. If intentional, '
-                             'pass extend_cost=True.')
+            raise ValueError(
+                    'Square cost array expected. If cost is intentionally '
+                    'non-square, pass extend_cost=True.')
+    if extend_cost or cost_limit < np.inf:
+        n = n_rows + n_cols
+        cost_c_extended = np.empty((n, n), dtype=np.double)
         if cost_limit < np.inf:
-            cost_c_extended = np.empty((2*N, 2*N), dtype=np.double)
-            cost_c_extended[:] = cost_limit
-            cost_c_extended[:N, :N] = cost_c.max() + cost_limit + 1
+            cost_c_extended[:] = cost_limit / 2.
         else:
-            cost_c_extended = np.empty((N, N), dtype=np.double)
             cost_c_extended[:] = cost_c.max() + 1
-        cost_c_extended[:cost_c.shape[0], :cost_c.shape[1]] = cost_c
-        cost_c = cost_c_extended
-    elif cost_limit < np.inf:
-        cost_c_extended = np.empty((2*N, 2*N), dtype=np.double)
-        cost_c_extended[:] = cost_limit
-        cost_c_extended[:cost_c.shape[0], :cost_c.shape[1]] = cost_c
+        cost_c_extended[n_rows:, n_cols:] = 0
+        cost_c_extended[:n_rows, :n_cols] = cost_c
         cost_c = cost_c_extended
 
-    cdef int i
     cdef double **cost_ptr
-    cost_ptr = <double **> malloc(cost_c.shape[0] * sizeof(double *))
-    for i in range(cost_c.shape[0]):
+    cost_ptr = <double **> malloc(n * sizeof(double *))
+    cdef int i
+    for i in range(n):
         cost_ptr[i] = &cost_c[i, 0]
 
     cdef cnp.ndarray[int_t, ndim=1, mode='c'] x_c = \
-        np.empty((cost_c.shape[0],), dtype=np.int32)
+        np.empty((n,), dtype=np.int32)
     cdef cnp.ndarray[int_t, ndim=1, mode='c'] y_c = \
-        np.empty((cost_c.shape[0],), dtype=np.int32)
+        np.empty((n,), dtype=np.int32)
 
-    cdef uint_t n = cost_c.shape[0]
     cdef int ret = lapjv_internal(n, cost_ptr, &x_c[0], &y_c[0])
     free(cost_ptr)
     if ret != 0:
@@ -100,16 +112,16 @@ def lapjv(cnp.ndarray cost not None, char extend_cost=False,
         raise RuntimeError('Unknown error (lapjv_internal returned %d).' % ret)
 
 
-    cdef double opt
-    if cost_limit < np.inf or extend_cost:
-        x_c[x_c >= cost.shape[1]] = -1
-        y_c[y_c >= cost.shape[0]] = -1
-        x_c = x_c[:cost.shape[0]]
-        y_c = y_c[:cost.shape[1]]
+    cdef double opt = np.nan
+    if n != n_rows:
+        x_c[x_c >= n_cols] = -1
+        y_c[y_c >= n_rows] = -1
+        x_c = x_c[:n_rows]
+        y_c = y_c[:n_cols]
         if return_cost:
             opt = cost_c[np.nonzero(x_c != -1)[0], x_c[x_c != -1]].sum()
     elif return_cost:
-        opt = cost_c[np.arange(N), x_c].sum()
+        opt = cost_c[np.arange(n_rows), x_c].sum()
 
     if return_cost:
         return opt, x_c, y_c
