86ac1311ae81706b8acd134de0372ed4f64ce61e
Tomas Kazmar <tomash.kazmar@seznam.cz>
2023-05-15 00:20:15 +0200

    Patch install of pythons

--- tools/install_pythons.sh
+++ tools/install_pythons.sh
@@ -4,7 +4,7 @@ apt-get update -y
 apt-get install -y software-properties-common
 add-apt-repository -y ppa:deadsnakes/ppa
 
-apt-get install python3-pip
+apt-get install -y python3-pip
 for PY_VER in 3.8 3.9 3.10 3.11; do
 	apt-get install -y \
 		"python${PY_VER}" \
