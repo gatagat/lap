11347ad700257f660aef8dc3cd67000579624e0d
Tomas Kazmar <tomash.kazmar@seznam.cz>
2023-05-17 23:13:15 +0200

    Install tzdata non-interactive

--- tools/install_pythons.sh
+++ tools/install_pythons.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 apt-get update -y
+DEBIAN_FRONTEND="noninteractive" apt-get -y install tzdata
+ln -fs /usr/share/zoneinfo/Europe/Vienna /etc/localtime
+dpkg-reconfigure --frontend noninteractive tzdata
 apt-get install -y software-properties-common
 add-apt-repository -y ppa:deadsnakes/ppa
 
@@ -13,3 +16,4 @@ for PY_VER in 3.8 3.9 3.10 3.11; do
 done
 
 apt-get clean
+rm -rf /var/lib/apt/lists/*
